aa119ed4456d86f9e3f7a99354d401e39a706bc8
Hector <hmarmar504@g.educaand.es>
2025-11-29 19:01:11 +0100

    PreciLuzCasa

---
+++ precioLuz/precioLuz.js
@@ -0,0 +1,10 @@
+const fechaLuz = document.getElementById("fechaLuz");
+const lineaLuz = document.getElementById("lineaLuz");
+const infoPrecioBajo= document.getElementById("precioBajo");
+const infoPrecioMedio = document.getElementById("precioMedio");
+const infoPrecioAlto = document.getElementById("precioAlto");
+const infoTramoEconomico = document.getElementById("tramoEconomico");
+
+fechaLuz.addEventListener("change",function(){
+const datosFecha = [];
+});
